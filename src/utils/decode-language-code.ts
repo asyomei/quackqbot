9b6504455dfa9e5b921851218d714024eb46e8f7
Vadim Abgalimov <52647771+vabgalimov@users.noreply.github.com>
2023-06-25 05:08:28 +0500

    Remove languageCodeRegexTest

--- src/utils/decode-language-code.ts
+++ src/utils/decode-language-code.ts
@@ -3,10 +3,6 @@ import regions from "../regions.json"
 const regionCodes = Object.keys(regions)
 const mem: Record<string, string | null> = {}
 
-const langCodeTestRegex = /^[a-z]{2}$/
-
 export function decodeLanguageCode(langCode: string): string | null {
-    if (!langCodeTestRegex.test(langCode))
-        return null
     return mem[langCode] ??= regionCodes.find(regionCode => regionCode.startsWith(langCode)) ?? null
 }
